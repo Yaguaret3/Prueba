4f559c3fd79f524496d496d8cede21f5799b9cb7
Yaguaret3 <64818001+Yaguaret3@users.noreply.github.com>
2021-09-03 18:31:50 -0300

    Update app.js
    Comentado

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 });
 
+// Conexión por webSocket y suscripción a "chat/sendMessage". Cada vez que alguien mande un mensaje a esa dirección se ejecuta la función pedirNumero().
 function conectar() {
     var socket = new SockJS('/chat-websocket');
     stompClient = Stomp.over(socket);
@@ -11,6 +12,7 @@ function conectar() {
     });
 }
 
+// Pedido restful por rest del número actual (estado de los recursos de la base, en el juego). Actualización de la pantalla con los datos actualizados del servidor (o base)
 async function pedirNumero(){
 
     const request = await fetch('pedirNumero', {
@@ -25,6 +27,7 @@ async function pedirNumero(){
     document.getElementById("numero").innerHTML = JSON.stringify(numeroPedido.valor);
 }
 
+// Función con dos métodos. 1) Fetch asincrónico restful para que el servidor ejecute la lógica pedida. 2) Ejecución de dispararWS()
 async function sumarUno() {
     const request = await fetch('sumarUno', {
        method: 'GET',
@@ -36,6 +39,7 @@ async function sumarUno() {
     dispararWS();
 }
 
+// Envío de mensaje por webSocket a la dirección "chat/sendMessage". Ejecuta pedirNumero() por estar suscripto automáticamente.
 function dispararWS(){
     stompClient.send('/chat/sendMessage', {}, JSON.stringify({'mensaje': "mensaje"}));
 }
